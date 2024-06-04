f99cd8414f0bb3262be987880d3f0683389a2801
kennaraya <arayakenneth934@gmail.com>
2024-06-04 10:58:18 -0600

    Estructura Repetitiva While

---
+++ src/Estructura_Repetitiva_While.kt
@@ -0,0 +1,22 @@
+fun main() {
+    /*
+    //Problema 1
+    var x = 1
+    while (x <= 100){
+        println(x)
+        x++
+    }
+     */
+
+    /*
+    //Problema 2
+    print("Ingrese un valor:")
+    val valor = readln().toInt()
+    var x = 1
+    while (x <= valor) {
+        println("x")
+        x++
+    }
+     */
+
+}
