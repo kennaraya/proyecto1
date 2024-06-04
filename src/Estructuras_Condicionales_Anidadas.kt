9aa1a30739efbbec8cc0fa4ccb9da8a9bcfd4676
kennaraya <arayakenneth934@gmail.com>
2024-06-04 10:13:05 -0600

    Estructuras Condicionales Anidadas

--- src/Estructuras_Condicionales_Anidadas.kt
+++ src/Estructuras_Condicionales_Anidadas.kt
@@ -34,6 +34,107 @@ fun main() {
     print("Estado del alumno $estado")
      */
 
+    /*
+    //problema propuesto 1
+    print("Ingrese el primer numero:")
+    val numero1 = readln().toInt()
+    print("Ingrese el segundo numero:")
+    val numero2 = readln().toInt()
+    print("Ingrese el tercer numero:")
+    val numero3 = readln().toInt()
+
+    if (numero1>numero2)
+        print("El numero mayor es $numero1")
+    else
+        if (numero2>numero3)
+            print("El numero mayor es $numero2")
+    else
+        print("El numero mayor es $numero3")
+     */
+
+    /*
+    //problema propuesto 3
+    print("Ingrese un numero del 1 al 999:")
+    val num: Int = readln().toInt()
+    if(num > 999)
+        print("ERROR, El numero de cifras es mayor")
+    else
+        if(num>99)
+            print("El numero tiene 3 cifras")
+    else
+        if(num>9)
+            print("El numero tiene 2 cifras")
+    else
+        print("El numero tiene una cifra")
+     */
+
+    /*
+//problema propuesto 4
+    print("Ingrese el numero de preguntas que realizo:")
+    val preguntastotal : Int = readln().toInt()
+    print("Ingrese el numero de preguntas contestadas correctamente:")
+    val preguntascorrectas : Int = readln().toInt()
+    val porcentaje: Float = preguntascorrectas/preguntastotal.toFloat()
+    println("El porcentaje es $porcentaje")
+    if(porcentaje >= 0.9)
+        print("Nivel maximo logrado")
+    if(porcentaje >= 0.75 && porcentaje < 0.9)
+        print("Nivel medio logrado")
+    if(porcentaje >= 0.5 && porcentaje < 0.75)
+        print("Nivel regular logrado")
+    else
+        print("Fuera de nivel")
+     */
+
+    /*
+  //documento 8
+    */
+
+    /*
+    //problema 1
+    print("Ingrese el dia:")
+    val dia = readln().toInt()
+    print("Ingrese el mes:")
+    val mes = readln().toInt()
+    print("Ingrese el año:")
+    val año = readln().toInt()
+    if(mes == 1 || mes == 2 || mes == 3){
+        println("$dia/$mes/$año pertenece al primer trimestre del año")
+    }else{
+        println("$dia/$mes/$año no pertenece al primer trimestre del año")
+    }
+     */
+
+    /*
+    //problema 2
+    print("Ingrese el primer valor")
+    val valor1 = readln().toInt()
+    print("Ingrese el segundo valor")
+    val valor2 = readln().toInt()
+    print("Ingrese el tercer valor")
+    val valor3 = readln().toInt()
+    if (valor1 == valor2 && valor1 == valor3){
+        val cubo = valor1*valor2*valor3
+        println("Los valores son iguales. El cubo es $cubo")
+    }else{
+        print("No se puede calcular el cubo de los valores ya que no son iguales.")
+    }
+    */
+
+    /*
+    //problema 3
+    print("Ingrese el valor del primer numero:")
+    val num1 = readln().toInt()
+    print("Ingrese el valor del segundo numero:")
+    val num2 = readln().toInt()
+    print("Ingrese el valor del tercer numero:")
+    val num3 = readln().toInt()
+    if(num1<10&&num2<10&&num3<10){
+        println("Todos los numeros son menores a 10")
+    }else{
+        println("Hay un numero mayor a 10")
+    }
+    */
 
 
 }
