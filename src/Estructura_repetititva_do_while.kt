d8ad5712e3bc439f41b009d09abcb198910ed5e4
kennaraya <arayakenneth934@gmail.com>
2024-06-10 16:04:13 -0600

    Estructuras while

---
+++ src/Estructura_repetititva_do_while.kt
@@ -0,0 +1,19 @@
+fun main(){
+    /*
+    //problema 1
+    do {
+        print("Ingrese un valor comprendido entre 0 y 999")
+        val valor = readln().toInt()
+        if(valor < 10)
+            println("El valor ingresado tiene un digito")
+        else
+            if(valor < 100)
+                println("El valor ingresado tiene dos digitos")
+            else
+                println("El valor ingresado tiene 3 digitos")
+    }while(valor != 0)
+     */
+
+
+
+}
